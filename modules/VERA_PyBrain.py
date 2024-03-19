89f40fba0ec3ecf5069be24e7ee2d3bb658a87e3
NB <nbryson2000@gmail.com>
2024-03-19 17:01:31 -0500

    loads VERA file and accesses each field, assigning it as an attribute to the VERA_PyBRain class. VERA_component class takes attributes that are dictionaries and turns their keys into their own attributes to avoid a jumble of string indexing. removed functions folder, made functions module for user functions to be populated later.

--- modules/VERA_PyBrain.py
+++ modules/VERA_PyBrain.py
@@ -1,15 +1,39 @@
 from pathlib import Path
 import scipy.io as scio
 
-class VERA_PyBrain:
-    def __init__(self,path2file:str):
+class VERA_PyBrain():
+    def __init__(self,path2file:str,subject:str=''):
         path=Path(path2file)
-        self.brain=self._loadMat(path)
-
+        if subject != '':
+            self.subject = subject
+        else:
+            self.subject = None
+        self._loadMat(path)
     def _loadMat(self,fp:Path):
-        temp = scio.loadmat(fp,mat_dtype=True,simplify_cells=True)
-        self.brain = 0
+        self.brainType = fp.name.split('.')[0]
+        try:
+            temp = scio.loadmat(fp,mat_dtype=True,simplify_cells=True)
+            for k,v in sorted(temp.items()):
+                if not isinstance(v,dict):
+                    self.__setattr__(k,v)
+                else:
+                    self.__setattr__(k,VERA_component(v))
+        except:
+            print('error loading brain :(')
+        else:
+            print('brain loaded successfully yay')
+        finally:
+            print('module complete')
 
+class VERA_component():
+    """class that recurses through dictionaries to make them into object variables rather than string keys for continuity with MATLAB struct interface"""
+    def __init__(self,input:dict):
+        for k,v in input.items():
+            if isinstance(v,dict):
+                self.__setattr__(k,VERA_component(v))
+            else:
+                self.__setattr__(k,v)
+                
 
 if __name__ == '__main__':
     loc = r'C:\Users\nbrys\Box\Brunner Lab\DATA\SCAN_Mayo\BJH041\brain\brain_MNI.mat'
---
+++ modules/functions.py
@@ -0,0 +1,6 @@
+
+
+class functions():
+    """Module containing useful functions for use in this entire package. Rather that importing specific functions they are all contained here"""
+    def __init__():
+        pass
